4068966d1b1ea93980e31cd4b31509134f9cbc66
ron <rigel.toth@gmail.com>
2024-04-30 18:43:33 -0500

    no auth

--- src/routes/form.ts
+++ src/routes/form.ts
@@ -2,10 +2,15 @@ import {Router} from "express"
 import { deleteForm, getForm, getFormAll, postForm, updateForm } from "../controllers/form";
 import { auth } from "../auth/auth";
 const router = Router();
-router.get('/:id',auth,getForm)
-router.get('/',auth,getFormAll)
-router.post('/',auth,postForm)
-router.put('/:id',auth,updateForm)
-router.delete('/:id',auth,deleteForm)
+// router.get('/:id',auth,getForm)
+// router.get('/',auth,getFormAll)
+// router.post('/',auth,postForm)
+// router.put('/:id',auth,updateForm)
+// router.delete('/:id',auth,deleteForm)
 
+router.get('/:id',getForm)
+router.get('/',getFormAll)
+router.post('/',postForm)
+router.put('/:id',updateForm)
+router.delete('/:id',deleteForm)
 export {router};
