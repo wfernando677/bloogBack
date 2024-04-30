ddff245f5cc225b8e9581240a6ed6e85294f71ce
ron <rigel.toth@gmail.com>
2024-04-30 17:23:50 -0500

    cleaning

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,16 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers);
-    if(req.headers.authorization===process.env.TOKEN){
+    if (req.headers.authorization === process.env.TOKEN) {
         next();
-        
-        
     }
-    else{
-        res.json({
-            msg:'No auth'
-        })
+    else {
+        res.json({ msg: 'No auth' })
     }
 
 };
